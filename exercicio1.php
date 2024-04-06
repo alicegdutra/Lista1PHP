c82dfc1af398f473bd47fd602414b83afbce2078
alicegdutra <alicegzdutra@gmail.com>
2024-04-06 10:38:01 -0300

    1

---
+++ exercicio1.php
@@ -0,0 +1,9 @@
+<?php
+
+    $valor = $_POST['valor']; //cria uma variável e recebe ela da página anterior
+    if($valor > 0)
+        echo "Valor positivo!";
+    elseif($valor < 0)
+        echo "Valor Negativo!";
+    else
+        echo "Valor igual a zero!";
---
+++ index1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="exercicio1.php" method="post">
+        <label>Informe o valor: </label> <!--o formulário que envia as informações para a outra pasta -->
+        <input type="number" name="valor">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
