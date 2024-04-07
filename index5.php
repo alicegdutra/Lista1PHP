bbbb56d662a471dc1d4c982d3941750d89b02e4a
Alice Gimenez Dutra <alicegzdutra@gmail.com>
2024-04-07 12:52:42 -0300

    exer5

---
+++ index5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercícios</title>
+</head>
+<body>
+    <form action="exercicio5.php" method="post">
+        <label>Informe um número: </label>
+        <input type="number" name="numero">
+        <button type="submit">Enviar</button>
+
+    </form>
+</body>
+</html>
